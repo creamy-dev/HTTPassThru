c220d8f318b43dc6e67223aebf37e125317b47b8
Greyson <56458201+creamy-dev@users.noreply.github.com>
2021-10-10 13:53:43 -0400

    Update runner.sh

--- runner.sh
+++ runner.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 echo HTTPassThru Auto-Updater.
 cp httpassthru/config.json . > /dev/null 2> /dev/null
-rm -rf httpassthru > /dev/null 2> /dev/null
+rm -rf HTTPassThru > /dev/null 2> /dev/null
 git clone https://github.com/creamy-dev/HTTPassThru.git
-rm -rf httpassthru/config.json
-mv config.json httpassthru/ 2> /dev/null > /dev/null
+rm -rf HTTPassThru/config.json
+mv config.json HTTPassThru/ 2> /dev/null > /dev/null
 cd HTTPassThru
 npm install
 echo Finished updating. Starting.
-npm .
+npm start
 echo Exiting...
 cd ..
